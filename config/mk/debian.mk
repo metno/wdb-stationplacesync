1347ce6b3227a75c2dce216a52f9ad053f99d7b6
Michael Akinde <michael.akinde@met.no>
2013-10-01 15:04:28 +0200

    Additional debian package corrections.

--- config/mk/debian.mk
+++ config/mk/debian.mk
@@ -1,11 +1,10 @@
-
 #-----------------------------------------------------------------------------
-# Common Debian Package
+# Debian stuff
 #-----------------------------------------------------------------------------
 
 PKG_DIR = $(PACKAGE)-$(VERSION)
 DEBIAN_DIR = $(PKG_DIR)/debian
-DEBIAN_PACKAGE = `head -n1 $(top_srcdir)/NEWS | sed "s/ (/_/" | sed "s/\(.*\)-.*/\1/"`
+DEBIAN_PACKAGE = `head -n1 $(top_srcdir)/debian_files/changelog | sed "s/ (/_/" | sed "s/_.*//"`_$(VERSION)
 DEBIAN_PACKAGE_NAME = *.deb
 DEBIAN_SOURCE_PACKAGE_NAME = *.dsc
 
@@ -17,7 +16,6 @@ prepare-debian:
 	rm -rf $(DEBIAN_DIR)
 	mkdir -p $(DEBIAN_DIR)
 	cp -r $(top_srcdir)/debian_files/* $(DEBIAN_DIR)
-	cp $(top_srcdir)/NEWS $(DEBIAN_DIR)/changelog
 	chmod 774 $(DEBIAN_DIR)/rules
 	if [ -e $(DEBIAN_DIR)/templates ]; then  debconf-updatepo --podir=$(DEBIAN_DIR)/po; fi
 
@@ -26,10 +24,10 @@ update-debian: prepare-debian
 	lintian $(DEBIAN_PACKAGE_NAME) $(DEBIAN_SOURCE_PACKAGE_NAME)
 
 build-debian:
-	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc -sa -i.svn
+	cd $(PKG_DIR) && dpkg-buildpackage -rfakeroot -us -uc -sa
 	lintian $(DEBIAN_PACKAGE_NAME) $(DEBIAN_SOURCE_PACKAGE_NAME)
 
-common-debian: dist-debian prepare-debian build-debian
+debian: dist-debian prepare-debian build-debian
 
 clean-debian:
 	debclean
